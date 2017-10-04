4a96261e73dbf427568e98ed007f1b807d9a0808
mrwarner09 <mrwarner09@gmail.com>
2017-10-04 14:48:57 -0400

    first commit

--- src/slurmParallel.sh
+++ src/slurmParallel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash#
-#SBATCH -p compute
 #SBATCH -N 1 # number of nodes
 #SBATCH -n 1 # number of cores
 #SBATCH --array=1-5
@@ -9,4 +8,4 @@
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --mail-type=END,FAIL # notifications for job done & fail
 #SBATCH --mail-user=warnerm@sas.upenn.edu # send-to addressfor i in {1..100000}; do
-Rscript analysis_parallelTests.R
+Rscript src/analysis_parallelTests.R
