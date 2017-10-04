41a29cffdf22e212837881c28525275547823e3f
mrwarner09 <mrwarner09@gmail.com>
2017-10-04 13:13:27 -0400

    working on resampling

--- src/analysis.R
+++ src/analysis.R
@@ -12,7 +12,10 @@ EdgeR <- function(data,design,coef){
   return(out)
 }
 
-filterLowly <- 
+filterLowly <- function(counts,tpm,cut){
+  keep = rownames(tpm)[rowSums(tpm > cut) > ncol(tpm)/2]
+  return(counts[keep,])
+}
 
 genFactor <- function(counts){
   factors <- data.frame(sample=colnames(counts))
@@ -27,9 +30,9 @@ genFactor <- function(counts){
   factors$tissue="larva"
   factors$tissue[grepl("P",factors$sample)]="pupa"
   factors$tissue[grepl("_E",factors$sample)]="egg"
-  factors$tissue[grepl("G.",factors$sample)]="gaster"
-  factors$tissue[grepl("H.",factors$sample)]="head"
-  factors$tissue[grepl("M.",factors$sample)]="mesosoma"
+  factors$tissue[grepl("G\\.",factors$sample)]="gaster"
+  factors$tissue[grepl("H\\.",factors$sample)]="head"
+  factors$tissue[grepl("M\\.",factors$sample)]="mesosoma"
   factors$NF=NA
   factors$NF[grepl("_N",factors$sample)]="nurse"
   factors$NF[grepl("_F",factors$sample)]="forager"
@@ -49,12 +52,25 @@ genFactor <- function(counts){
   return(factors)
 }
 
+tissues <- c("head","gaster","mesosoma")
+
 bee <- read.table("~/GitHub/devnetwork/data/bees.counts_edit.txt",header=TRUE)
 ant <- read.table("~/GitHub/devnetwork/data/ants.counts_edit.txt",header=TRUE)
 rownames(bee) = bee$gene
 rownames(ant) = ant$gene
-bee = bee[,-c(1)]
-ant = ant[,-c(1)]
+bee = bee[!grepl("ERCC",bee$gene),-c(1)]
+ant = ant[!grepl("ERCC",ant$gene),-c(1)]
+
+beeT <- read.table("~/GitHub/devnetwork/data/bees.tpm_edit.txt",header=TRUE)
+antT <- read.table("~/GitHub/devnetwork/data/ants.tpm_edit.txt",header=TRUE)
+rownames(beeT) = beeT$gene
+rownames(antT) = antT$gene
+beeT <- beeT[!grepl("ERCC",beeT$gene),-c(1)]
+antT <- antT[!grepl("ERCC",antT$gene),-c(1)]
+
+#Filter out lowly expressed genes
+#ant_filt <- filterLowly(ant,antT,0.01)
+#bee_filt <- filterLowly(bee,beeT,0.01)
 
 ogg2 <- read.csv("~/GitHub/devnetwork/data/HymOGG_hym.csv",sep=" ")
 ogg3 <- read.csv("~/GitHub/devnetwork/data/ThreeWayOGGMap.csv",header=TRUE)
@@ -91,8 +107,179 @@ genDevTool <- function(factors,data){
 BeeDev <- genDevTool(factorB,bee)
 AntDev <- genDevTool(factorA,ant)
 
+BeeOGG <- ogg2$OGG[ogg2$gene_Amel %in% rownames(bee)]
+AntOGG <- ogg2$OGG[ogg2$gene_Mphar %in% rownames(ant)]
+commonOGG <- BeeOGG[BeeOGG %in% AntOGG]
+
 BeeDevOGG <- ogg2$OGG[ogg2$gene_Amel %in% BeeDev]
 AntDevOGG <- ogg2$OGG[ogg2$gene_Mphar %in% AntDev]
 TwoSpecDev = BeeDevOGG[BeeDevOGG %in% AntDevOGG]
+
+DevList <- list(BeeDev,AntDev,TwoSpecDev)
+nGene <- list(nrow(bee),nrow(ant),length(commonOGG))
+
+##########
+##Caste toolkit
+tissueCaste <- function(factors,data,tissue,scramble=FALSE){
+  counts <- data[,factors$stage==8&factors$tissue==tissue&factors$caste!="male"]
+  f = droplevels(factors[factors$sample %in% colnames(counts),])
+  if (scramble){ #mix up caste labels
+    f$caste = sample(f$caste,length(f$caste),replace=F)
+  }
+  design <- model.matrix(~caste+colony,data=f)
+  out <- EdgeR(counts,design,2)
+  return(rownames(out)[out$FDR < 0.05])
+}
+
+tissues <- c("head","gaster","mesosoma")
+beeCaste <- list()
+antCaste <- list()
+TwoSpecCaste <- list()
+
+for (tissue in tissues){
+  beeCaste[[tissue]]=tissueCaste(factorB,bee_filt,tissue)
+  antCaste[[tissue]]=tissueCaste(factorA,ant_filt,tissue)
+  beeOGG <- ogg2$OGG[ogg2$gene_Amel %in% beeCaste[[tissue]]]
+  antOGG <- ogg2$OGG[ogg2$gene_Mphar %in% antCaste[[tissue]]]
+  TwoSpecCaste[[tissue]] <- beeOGG[beeOGG %in% antOGG]
+}
+
+Castelist <- list(beeCaste,antCaste,TwoSpecCaste)
+
+
+#############
+##Social toolkit
+tissueSocial <- function(factors,data,tissue,scramble=FALSE){
+  counts <- data[,factors$stage==8&factors$tissue==tissue&factors$caste=="worker"]
+  f = droplevels(factors[factors$sample %in% colnames(counts),])
+  if (scramble){ #mix up caste labels
+    f$NF = sample(f$NF,length(f$NF),replace=F)
+  }
+  design <- model.matrix(~NF+colony,data=f)
+  out <- EdgeR(counts,design,2)
+  return(rownames(out)[out$FDR < 0.05])
+}
+beeNF <- list()
+antNF <- list()
+TwoSpecNF <- list()
+
+for (tissue in tissues){
+  beeNF[[tissue]]=tissueSocial(factorB,bee,tissue)
+  antNF[[tissue]]=tissueSocial(factorA,ant,tissue)
+  beeOGG <- ogg2$OGG[ogg2$gene_Amel %in% beeNF[[tissue]]]
+  antOGG <- ogg2$OGG[ogg2$gene_Mphar %in% antNF[[tissue]]]
+  TwoSpecNF[[tissue]] <- beeOGG[beeOGG %in% antOGG]
+}
+
+NFlist <- list(beeNF,antNF,TwoSpecNF)
+
+##########
+##Bootstrapping the whole process
+bootOverlap <- function(antF,beeF,antC,beeC,test){
+  bee <- list()
+  ant <- list()
+  TwoSpec <- list()
+
+  for (tissue in tissues){
+    if (test == "caste"){
+      bee[[tissue]]=tissueCaste(beeF,beeC,tissue,scramble=TRUE)
+      ant[[tissue]]=tissueCaste(antF,antC,tissue,scramble=TRUE)
+    } else {
+      bee[[tissue]]=tissueSocial(beeF,beeC,tissue,scramble=TRUE)
+      ant[[tissue]]=tissueSocial(antF,antC,tissue,scramble=TRUE)
+    }
+    beeOGG <- ogg2$OGG[ogg2$gene_Amel %in% bee[[tissue]]]
+    antOGG <- ogg2$OGG[ogg2$gene_Mphar %in% ant[[tissue]]]
+    TwoSpec[[tissue]] <- beeOGG[beeOGG %in% antOGG]
+  }
+  
+  AllTest <- list(bee,ant,TwoSpec)
+  return(calcOverlap(AllTest))
+  
+}
+
+calcOverlap <- function(AllTest){
+  overlap = matrix(nrow=1,ncol=9)
+  for (i in 1:3){
+    for (j in 1:3){
+      overlap[1,(i-1)*3+j] = sum(AllTest[[i]][[tissues[j]]] %in% DevList[[i]])
+    }
+  }
+  colnames(overlap) = apply(expand.grid(tissues,c("bee","ant","overlap")), 1, paste, collapse=".")
+  return(overlap)
+}
+
+runBoots <- function(antF,beeF,antC,beeC,test,nBoot){
+  results <- matrix(nrow=1,ncol=9)
+  colnames(results) = apply(expand.grid(tissues,c("bee","ant","overlap")), 1, paste, collapse=".")
+  i = 1
+  while (i <= nBoot){ 
+    x <- try(bootOverlap(antF,beeF,antC,beeC,test)) ##Protects from annoying errors that happen with resampling
+    if (!inherits(x,"try-error")){
+      results = rbind(results,x)
+      i = i+1
+    } 
+  }
+  return(results[-c(1),])
+}
+
+bootCaste <- runBoots(factorA,factorB,ant,bee,"caste",50)
+bootSocial <- runBoots(factorA,factorB,ant,bee,"social",50)
+
+
+#######
+##Hypothesis tests
+######Make table with # of genes in each of the lists above (not developmental), plus number of genes in analysis (in parentheses)
+######Then add number of genes in each overlap comparison
+######Rows are head, mesosoma, and gaster for both caste and social
+######Can do a quick chi square, but also do bootstrapping for confidence intervals based on scrambled data
+test_list <- list(Castelist,NFlist)
+
+table <- matrix(nrow = 8, ncol = 9)
+for (i in 1:2){
+  for (j in 1:3){
+    for (k in 1:3){
+      table[(i-1)*3+j,k] = length(test_list[[i]][[k]][[tissues[j]]])
+    }
+  }
+}
+
+for (i in 1:3){
+  table[7,i] = length(DevList[[i]])
+  table[7,i+3] = length(DevList[[i]])
+  table[7,i+6] = length(DevList[[i]])
+  table[8,i] = nGene[[i]]
+  table[8,i+3] = nGene[[i]]
+  table[8,i+6] = nGene[[i]]
+}
+
+
+
+for (i in 1:3){
+  for (j in 1:2){
+    for (k in 1:3){
+      table[(j-1)*3+k,i+3] = sum(DevList[[i]] %in% test_list[[j]][[i]][[tissues[k]]])
+      chi = rbind(c(table[(j-1)*3+k,i+3],
+                    table[(j-1)*3+k,i]-table[(j-1)*3+k,i+3]),
+                  c(table[7,i] - table[(j-1)*3+k,i+3],
+                    nGene[[i]] - table[7,i] - table[(j-1)*3+k,i] + table[(j-1)*3+k,i+3]))
+      table[(j-1)*3+k,i+6] = fisher.test(chi,alternative="greater")$p.value
+    }
+  }
+}
+
+rownames(table) <- c(apply(expand.grid(tissues,c("Caste","NF")), 1, paste, collapse="."),"DevelToolkit","nGene_Analysis")
+colnames(table) <- c("Amel","Mpharaonis","TwoSpecies","Amel_Devel",
+                     "Mpharaonis_Devel","TwoSpecies_Devel",
+                     "Amel_chiP","Mpharaonis_chiP","TwoSpecies_chiP")
+
+png("~/GitHub/devnetwork/results/OverlapTable.png")
+grid.table()
+
+write.table(table,file="~/GitHub/devnetwork/results/OverlapTable.txt",row.names=TRUE)
+            
+                
+######Then want list of overlap genes and GO terms if possible
+
 
 
