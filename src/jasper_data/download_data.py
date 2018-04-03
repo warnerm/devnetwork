314a6f19e988fd9c74ac7d7bc387687d04ed82a9
warnerm <mrwarner09@gmail.com>
2018-04-03 12:44:57 -0400

    file working

---
+++ src/jasper_data/download_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#SBATCH -p compute # partition (queue)
+#SBATCH --export=ALL
+#SBATCH -t 10-00:00
+#SBATCH -n 1
+
+import pandas as pd 
+from subprocess import call
+sra = pd.read_table('samples_jasper.txt')
+keys = sra.iloc[:,3]
+
+for key in keys:
+	call(["fastq-dump",key])
