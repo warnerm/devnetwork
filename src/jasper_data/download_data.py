39dd9079c33ef08a9c4fcf5af3a9e95c8eba76fe
warnerm <mrwarner09@gmail.com>
2018-04-04 10:06:04 -0400

    added more fastq-dump commands

--- src/jasper_data/download_data.py
+++ src/jasper_data/download_data.py
@@ -9,5 +9,6 @@ from subprocess import call
 sra = pd.read_table('samples_jasper.txt')
 keys = sra.iloc[:,3]
 
+#Fastq-dump commands from https://edwards.sdsu.edu/research/fastq-dump/
 for key in keys:
-	call(["fastq-dump",key])
+	call(["fastq-dump",key,"gzip","--split-3","--read-filter","pass","--outdir","fastq","--skip-technical"])
