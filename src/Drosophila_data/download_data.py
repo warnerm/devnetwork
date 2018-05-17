29a25f6375ef2a2aa5b544fea645ff10c218e8e4
warnerm <mrwarner09@gmail.com>
2018-05-17 13:58:49 -0400

    first commit

---
+++ src/Drosophila_data/download_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#SBATCH -p compute # partition (queue)
+#SBATCH --export=ALL
+#SBATCH -t 10-00:00
+#SBATCH -n 5
+
+import pandas as pd 
+from subprocess import call
+sra = pd.read_table('drosophila_sra.csv')
+keys = sra.iloc[:,0]
+
+#Fastq-dump commands from https://edwards.sdsu.edu/research/fastq-dump/
+for key in keys:
+	call(["fastq-dump",key,"gzip","--split-3","--read-filter","pass","--outdir","fastq","--skip-technical"])
