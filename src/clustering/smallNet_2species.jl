3e3539947d77002f0f70637197edec2f1b140c1e
warnerm <mrwarner09@gmail.com>
2018-06-28 15:21:21 -0400

    Fixed weighting for orthologs

--- src/clustering/smallNet_2species.jl
+++ src/clustering/smallNet_2species.jl
@@ -1,18 +1,18 @@
 using Distributions
 #using DistributedArrays
 
-# addprocs(parse(Int,ARGS[4]))
-addprocs(2)
+addprocs(parse(Int,ARGS[6]))
 #
-# input1 = ARGS[1]
-# input2 = ARGS[2]
-# output1 = ARGS[3]
-# output2 = ARGS[4]
-input2 = "../../../../Data/devnetwork/beeNetSmall.csv"
-input1 = "../../../../Data/devnetwork/ant100_net.csv"
-output2 = "../../../../Data/devnetwork/beeNetSmall_clust.csv"
-output1 = "../../../../Data/devnetwork/ant100_net_clust.csv"
-OGGfile = "../../../../Data/devnetwork/OGGmap.txt"
+input1 = ARGS[1]
+input2 = ARGS[2]
+output1 = ARGS[3]
+output2 = ARGS[4]
+OGGmap = ARGS[5]
+# input2 = "../../../../Data/devnetwork/beeNetSmall.csv"
+# input1 = "../../../../Data/devnetwork/ant100_net.csv"
+# output2 = "../../../../Data/devnetwork/beeNetSmall_clust.csv"
+# output1 = "../../../../Data/devnetwork/ant100_net_clust.csv"
+# OGGmap = "../../../../Data/devnetwork/OGGmap.txt"
 
 #First, find number of genes
 function getNgene(file)
@@ -30,13 +30,13 @@ end
 
 #From file giving list of directed connections, make an adjacency matrix
 function getAdj(file,nGene)
-    Adj = falses(nGene,nGene)
+    Adj = zeros(nGene,nGene)
     open(file) do f
         for ln in eachline(file)
             sp = split(ln,"\t")
             a1 = parse(Int,sp[1]) + 1
             a2 = parse(Int,sp[2]) + 1
-            Adj[a1,a2] = true
+            Adj[a1,a2] = 1
         end
     end
     return Adj
@@ -57,21 +57,29 @@ function calcPartial(node,indiv_spin,net)
     @eval @everywhere s = $indiv_spin
     @eval @everywhere n = $net
     energy = pmap((args)->nodeEnergy(args...),[j for j=1:nGene[net]])
+    energy_all = sum(energy)
     #Add energy if orthologs are in the same module
-    ortholog = get(dict[net],node,-1)
-    if ortholog != - 1 && ortholog <= nGene[3-net]
-        if spins[3-net][ortholog] == spins[net][node]
-            energy = energy + coupling_constant*weights[net][node]
+    if haskey(dict[net],node)
+        OGG = dict[net][node]
+        partners = dict_ogg[3-net][OGG]
+        println(partners)
+        for i=1:length(partners)
+            if partners[i] != 0 && partners[i] <= nGene[3-net]
+                if spins[3-net][partners[i]] == spins[net][node]
+                    println("ortholog found")
+                    energy_all = energy_all + coupling_constant*weights[OGG]
+                end
+            end
         end
     end
-    return -sum(energy)
+    return -energy_all
 end
 
 @everywhere begin
     #Objective function to test energy with respect to given node
     function nodeEnergy(j)
         if spins[n][j] ==  s
-            return ((Adj_all[n][i,j]+Adj_all[n][j,i]) -  2*pos_each[n][i]*pos_each[n][j]/(2*tot_pos[n]))
+            return (Adj_all[n][i,j]+Adj_all[n][j,i] -  pos_out[n][i]*pos_in[n][j]/(2*tot_pos[n]) - pos_in[n][i]*pos_out[n][j]/(2*tot_pos[n]))
         else
             return 0
         end
@@ -81,17 +89,27 @@ end
 function move()
     net = rand(1:2)
     node = rand(1:nGene[net])
-    edit_spin = rand(1:max_mods)
+    edit_spin = copy(spins[net][node])
+    while edit_spin == spins[net][node]
+        edit_spin = rand(1:max_mods)
+    end
     oldEnergy = calcPartial(node,spins[net][node],net)
+    println("old energy is $oldEnergy")
     newEnergy = calcPartial(node,edit_spin,net)
+    println("new energy is $newEnergy")
     passed = 0
     @eval @everywhere test_node = $node
     if newEnergy < oldEnergy
+        println("passed automatically")
         @eval @everywhere spins[$net][test_node] = $edit_spin
         passed = 1
     else
-        prob = exp((oldEnergy - newEnergy)/(temp))
-        if prob > rand(Uniform(0,1),1)[1]
+        prob = exp(-(newEnergy - oldEnergy)/(temp))
+        println("prob is $prob")
+        test_num = rand(Uniform(0,1))
+        println("test_num is $test_num")
+        if prob > test_num
+            println("passed")
             @eval @everywhere spins[$net][test_node] = $edit_spin
             passed = 1
         end
@@ -100,36 +118,43 @@ function move()
 end
 
 function genDictionary(OGGfile)
-    dict1 = Dict{Int64,Int64}()
-    dict2 = Dict{Int64,Int64}()
-    weights1 = Dict{Int64,Float64}()
-    weights2 = Dict{Int64,Float64}()
+    dict1 = Dict{Int64,String}()
+    dict2 = Dict{Int64,String}()
+    ogg1 = Dict{String,Vector{Int64}}()
+    ogg2 = Dict{String,Vector{Int64}}()
+    weights = Dict{String,Float64}()
     open(OGGfile) do f
         for ln in eachline(f)
             sp = split(ln,"\t")
-            a1 = parse(Int,sp[1])
-            a2 = parse(Int,sp[2])
-            a3 = parse(Float,sp[3])
-            dict1[a1] = a2
-            dict2[a2] = a1
-            weights1[a1] = a3
-            weights2[a2] = a3
-
+            a1 = parse(Int,sp[1])+1
+            a2 = parse(Int,sp[2])+1
+            a3 = sp[3]
+            a4 = parse(Float64,sp[4])
+            dict1[a1] = a3
+            dict2[a2] = a3
+            if haskey(ogg1,a3)
+                ogg1[a3] = push!(ogg1[a3],a1)
+                ogg2[a3] = push!(ogg1[a3],a2)
+            else
+                ogg1[a3] = [a1]
+                ogg2[a3] = [a2]
+            end
+            weights[a3] = a4
         end
     end
     all_dict = [dict1,dict2]
-    weights = [weights1,weights2]
-    return all_dict,weights
+    all_ogg = [ogg1,ogg2]
+    return all_dict,all_ogg,weights
 end
 
-temp = 0.1
-epochs = 5
+temp = 10
+epochs = 100
 max_mods = 250
 coupling_constant = 3
 srand()
 
 #Functions to update temperature and number of iterations per epoch
-tf(t) = 0.7*t
+tf(t) = 0.9*t
 itf(length) = floor(Int,1.0*length)
 
 nGene1 = getNgene(input1)
@@ -138,19 +163,21 @@ Adj1 = getAdj(input1,nGene1)
 Adj2 = getAdj(input2,nGene2)
 
 #Load orthology, make a dictionary
-dict,weights = genDictionary(OGGfile)
+dict,dict_ogg,weights = genDictionary(OGGmap)
 
 
 @eval @everywhere nGene = [$nGene1,$nGene2]
 @eval @everywhere Adj_all = [$Adj1,$Adj2]
 
 @everywhere tot_pos = [sum(Adj_all[j]) for j=1:2]
-@everywhere pos_each = [sum(Adj_all[j],1) for j=1:2]
+@everywhere pos_in = [sum(Adj_all[j],1) for j=1:2]
+@everywhere pos_out = [sum(Adj_all[j],2) for j=1:2]
 spin_list = [Initialize(nGene[i]) for i=1:2]
 @eval @everywhere spins = $spin_list
 
 
-for iter=1:2
+for iter=1:10000
+    println(temp)
     success = 0
     for e=1:epochs
         passed = move()
