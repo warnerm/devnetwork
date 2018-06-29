37d8fdc0c3fb9c0bd75652ed58024a45954d7b7b
warnerm <mrwarner09@gmail.com>
2018-06-29 15:42:48 -0400

    filtering for genes in DE tests

--- src/clustering/singleNet.R
+++ src/clustering/singleNet.R
@@ -1,21 +1,19 @@
 #Will use IndivNet.py to determine single-species networks
 #This script takes in tpm matrices and outputs pearson correlation matrices
 makeCor <- function(species){
-  tpm <- read.table(paste("~/GitHub/devnetwork/data/",species,".tpm.txt",sep=""),header=TRUE)
-  tpm <- tpm[!grepl("ERCC",tpm$id),]
-  rownames(tpm) = tpm$id
-  tpm = tpm[c(1:100),-c(1)]
-  tpm = tpm[rowSums(tpm) > 0,]
+  tpm <- read.csv(paste("../data/",species,"_filt.tpm.csv",sep=""))
+  tpm = tpm[,-c(1)]
+  
   tCor = cor(t(tpm))
   tCor = round(tCor,3)
-  write.csv(tCor,paste("~/Data/devnetwork/",species,"SMALLpCor.csv",sep=""),row.names=FALSE)
+  #write.csv(tCor,paste("~/Data/devnetwork/",species,"SMALLpCor.csv",sep=""),row.names=FALSE)
   # write.csv(tCor[1:1000,1:1000],paste("~/Data/devnetwork/",species,"TESTpCor.csv",sep=""),row.names=FALSE)
-  # write.csv(tCor,paste("~/Data/devnetwork/",species,"pCor.csv",sep=""),row.names=FALSE)
+  write.csv(tCor,paste("../out/",species,"pCor.csv",sep=""),row.names=FALSE)
   return(0)
 }
 
-makeCor("bees")
-makeCor("ants")
+makeCor("bee")
+makeCor("ant")
 
 ###Prepare data for OrthoClust
 # prepOrthoClust <- function(species,d){
