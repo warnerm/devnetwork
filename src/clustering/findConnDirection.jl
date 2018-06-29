7a0dff9d322a754039a20e754a0f352af285659a
warnerm <mrwarner09@gmail.com>
2018-06-29 15:10:29 -0400

    stopped skipping first line

--- src/clustering/findConnDirection.jl
+++ src/clustering/findConnDirection.jl
@@ -1,10 +1,10 @@
-# inputfile = "../../../../Data/devnetwork/ant100_net.csv"
-# adjFile = "../../../../Data/devnetwork/antsSMALLpCor.csv"
-# outPre = "../../../../Data/devnetwork/antSmall"
-
-inputfile = ARGS[1]
-adjFile = ARGS[2]
-outPre = ARGS[3]
+inputfile = "../../../../Data/devnetwork/ant100_net.csv"
+adjFile = "../../../../Data/devnetwork/antsSMALLpCor.csv"
+outPre = "../../../../Data/devnetwork/antSmall"
+#
+# inputfile = ARGS[1]
+# adjFile = ARGS[2]
+# outPre = ARGS[3]
 
 outPos = string(outPre,"pos.txt")
 outNeg = string(outPre,"neg.txt")
@@ -16,24 +16,19 @@ end
 open(inputfile) do f
     counter = 0
     for line in eachline(f)
-        if counter == 0
-            counter = 1
-            continue
+        sp = split(line,"\t")
+        a1 = parse(Int,sp[1])+1
+        a2 = parse(Int,sp[2])+1
+        corList = open(readlines,adjFile)[a1+1]
+        adj = split(corList,",")
+        adjT = parse(Float64,adj[a2])
+        if adjT < 0
+            open(outNeg,"a") do out
+                write(out,line,"\n")
+            end
         else
-            sp = split(line,"\t")
-            a1 = parse(Int,sp[1])+1
-            a2 = parse(Int,sp[2])+1
-            corList = open(readlines,adjFile)[a1+1]
-            adj = split(corList,",")
-            adjT = parse(Float64,adj[a2])
-            if adjT < 0
-                open(outNeg,"a") do out
-                    write(out,line,"\n")
-                end
-            else
-                open(outPos,"a") do out
-                    write(out,line,"\n")
-                end
+            open(outPos,"a") do out
+                write(out,line,"\n")
             end
         end
     end
