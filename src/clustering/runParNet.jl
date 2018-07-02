17f50ec82f6eade272fe6e4b8c8c2ccae9b552eb
warnerm <mrwarner09@gmail.com>
2018-07-02 13:32:02 -0400

    got parallel network construction going

---
+++ src/clustering/runParNet.jl
@@ -0,0 +1,78 @@
+addprocs(2)
+
+# addprocs(parse(Int,ARGS[6]))
+# #
+# input1 = ARGS[1]
+# input2 = ARGS[2]
+# OGGmap = ARGS[3]
+# @eval @everywhere output1 = $ARGS[4]
+# @eval @everywhere output2 = $ARGS[5]
+# boots = ARGS[7]
+
+#
+input2 = "../../../../Data/devnetwork/beeSmall"
+input1 = "../../../../Data/devnetwork/antSmall"
+OGGmap = "../../../../Data/devnetwork/OGGmap.txt"
+@everywhere output1 = "../../../../Data/devnetwork/antTest"
+@everywhere output2 = "../../../../Data/devnetwork/beeTest"
+@everywhere include("smallNet_2species.jl")
+
+boots=3
+
+pos1 = string(input1,"pos.txt")
+neg1 = string(input1,"neg.txt")
+pos2 = string(input2,"pos.txt")
+neg2 = string(input2,"neg.txt")
+
+
+@everywhere initial_temp = 1
+@everywhere epochs = 10
+@everywhere max_mods = 250
+@everywhere coupling_constant = 3
+@everywhere cooling_constant = 0.9
+
+nGene1 = getNgene(pos1)
+nGene1n = getNgene(neg1)
+nGene1 = max(nGene1,nGene1n)
+nGene2 = getNgene(pos2)
+nGene2n = getNgene(neg2)
+nGene2 = max(nGene2,nGene2n)
+Adj1pos = getAdj(pos1,nGene1)
+Adj1neg = getAdj(neg1,nGene1)
+Adj2pos = getAdj(pos2,nGene2)
+Adj2neg = getAdj(neg2,nGene2)
+@eval @everywhere nGene = [$nGene1,$nGene2]
+
+
+#Load orthology, make a dictionary
+dict1,dict_ogg1,weights = genDictionary(OGGmap)
+AdjO = OGGmat(dict1,dict_ogg1,weights)
+@eval @everywhere dict = $dict1
+@eval @everywhere dict_ogg = $dict_ogg1
+@eval @everywhere AdjMatOGG = $AdjO
+
+Adj_pos = [diagMat(M) for M in [Adj1pos,Adj2pos]]
+Adj_neg = [diagMat(M) for M in [Adj1neg,Adj2neg]]
+
+tot_pos = [sum(Adj_pos[j]) for j=1:2]
+pos = [sum(Adj_pos[j],2) for j=1:2]
+pos_adj = [(pos[i]*pos[i]')/(2*tot_pos[i]) for i=1:2]
+
+tot_neg = [sum(Adj_neg[j]) for j=1:2]
+neg = [sum(Adj_neg[j],2) for j=1:2]
+neg_adj = [(neg[i]*neg[i]')/(2*tot_neg[i]) for i=1:2]
+
+#We are basically just going to be evaluating the contribution of a pair of nodes to the network, so may as well
+#compute the whole thing before hand
+AdjMat2 = [Adj_pos[i]-Adj_neg[i]-pos_adj[i]+neg_adj[i] for i=1:2]
+for n=1:2
+    for i=1:nGene[n],j=1:nGene[n]
+        if i == j
+            AdjMat2[n][i,j] = 0
+        end
+    end
+end
+
+@eval @everywhere AdjMat = $AdjMat2
+
+pmap((args)->runSim(args...),[rn for rn=1:boots])
--- src/clustering/smallNet_2species.jl
+++ src/clustering/smallNet_2species.jl
@@ -1,24 +1,6 @@
-using Distributions
+@everywhere using Distributions
 #using DistributedArrays
 #
-# addprocs(parse(Int,ARGS[4]))
-#
-input1 = ARGS[1]
-input2 = ARGS[2]
-OGGmap = ARGS[3]
-
-#
-# input2 = "../../../../Data/devnetwork/beeSmall"
-# input1 = "../../../../Data/devnetwork/antSmall"
-# OGGmap = "../../../../Data/devnetwork/OGGmap.txt"
-
-pos1 = string(input1,"pos.txt")
-neg1 = string(input1,"neg.txt")
-pos2 = string(input2,"pos.txt")
-neg2 = string(input2,"neg.txt")
-output1 = string(input1,"net_clust.csv")
-output2 = string(input2,"net_clust.csv")
-
 #First, find number of genes
 function getNgene(file)
     lastGene = Int
@@ -48,16 +30,18 @@ function getAdj(file,nGene)
 end
 
 #Intialize spins with random modules
-function Initialize(nGene)
-    spin = Vector{UInt8}(nGene)
-    for i=1:nGene
-        spin[i] = convert(UInt8,rand(1:max_mods))
+begin
+    @everywhere function Initialize(nGene)
+        spin = Vector{UInt8}(nGene)
+        for i=1:nGene
+            spin[i] = convert(UInt8,rand(1:max_mods))
+        end
+        return spin
     end
-    return spin
 end
 
 #calculate the energy for a given node/spin, given the rest of the network as is
-function calcPartial(node,indiv_spin,net)
+@everywhere function calcPartial(spins,node,indiv_spin,net)
     sameMod = [j for j in  1:nGene[net] if spins[net][j] == indiv_spin]
     if length(sameMod) > 0
         energy_all = sum([AdjMat[net][node,j] for j in sameMod])
@@ -80,28 +64,30 @@ function calcPartial(node,indiv_spin,net)
     return -energy_all
 end
 
-function move()
-    net = rand(1:2)
-    node = rand(1:nGene[net])
-    edit_spin = copy(spins[net][node])
-    while edit_spin == spins[net][node]
-        edit_spin = convert(UInt8,rand(1:max_mods))
-    end
-    oldEnergy = calcPartial(node,spins[net][node],net)
-    newEnergy = calcPartial(node,edit_spin,net)
-    passed = 0
-    if newEnergy < oldEnergy
-        spins[net][node] = edit_spin
-        passed = 1
-    else
-        prob = exp(-(newEnergy - oldEnergy)/(temp))
-        test_num = rand(Uniform(0,1))
-        if prob > test_num
+begin
+    @everywhere function move(spins,temp)
+        net = rand(1:2)
+        node = rand(1:nGene[net])
+        edit_spin = copy(spins[net][node])
+        while edit_spin == spins[net][node]
+            edit_spin = convert(UInt8,rand(1:max_mods))
+        end
+        oldEnergy = calcPartial(spins,node,spins[net][node],net)
+        newEnergy = calcPartial(spins,node,edit_spin,net)
+        passed = 0
+        if newEnergy < oldEnergy
             spins[net][node] = edit_spin
             passed = 1
+        else
+            prob = exp(-(newEnergy - oldEnergy)/(temp))
+            test_num = rand(Uniform(0,1))
+            if prob > test_num
+                spins[net][node] = edit_spin
+                passed = 1
+            end
         end
+        return passed
     end
-    return passed
 end
 
 function genDictionary(OGGfile)
@@ -157,70 +143,28 @@ function OGGmat(dict,dict_ogg,weights)
     return matO
 end
 
-temp = .05
-epochs = 100
-max_mods = 250
-coupling_constant = 3
-srand()
-
-#Functions to update temperature and number of iterations per epoch
-tf(t) = 0.9*t
-itf(length) = floor(Int,1.0*length)
-
-nGene1 = getNgene(pos1)
-nGene1n = getNgene(neg1)
-nGene1 = max(nGene1,nGene1n)
-nGene2 = getNgene(pos2)
-nGene2n = getNgene(neg2)
-nGene2 = max(nGene2,nGene2n)
-Adj1pos = getAdj(pos1,nGene1)
-Adj1neg = getAdj(neg1,nGene1)
-Adj2pos = getAdj(pos2,nGene2)
-Adj2neg = getAdj(neg2,nGene2)
-nGene = [nGene1,nGene2]
-
-
-#Load orthology, make a dictionary
-dict,dict_ogg,weights = genDictionary(OGGmap)
-AdjMatOGG = OGGmat(dict,dict_ogg,weights)
-
-Adj_pos = [diagMat(M) for M in [Adj1pos,Adj2pos]]
-Adj_neg = [diagMat(M) for M in [Adj1neg,Adj2neg]]
-
-tot_pos = [sum(Adj_pos[j]) for j=1:2]
-pos = [sum(Adj_pos[j],2) for j=1:2]
-pos_adj = [(pos[i]*pos[i]')/(2*tot_pos[i]) for i=1:2]
-
-tot_neg = [sum(Adj_neg[j]) for j=1:2]
-neg = [sum(Adj_neg[j],2) for j=1:2]
-neg_adj = [(neg[i]*neg[i]')/(2*tot_neg[i]) for i=1:2]
-
-#We are basically just going to be evaluating the contribution of a pair of nodes to the network, so may as well
-#compute the whole thing before hand
-AdjMat = [Adj_pos[i]-Adj_neg[i]-pos_adj[i]+neg_adj[i] for i=1:2]
-for n=1:2
-    for i=1:nGene[n],j=1:nGene[n]
-        if i == j
-            AdjMat[n][i,j] = 0
+begin
+    @everywhere function runSim(run)
+        srand()
+        spins = [Initialize(nGene[i]) for i=1:2]
+        temp = initial_temp
+        for i=1:10
+            success = 0
+            for e=1:epochs
+                passed = move(spins,temp)
+                success = success+passed
+            end
+            println(success/epochs)
+            if success/epochs < 0.01 #Stop iterations if there are very few successes
+                break
+            end
+            temp = temp*0.9
         end
+
+        sOut1 = [1:nGene[1] spins[1]]
+        sOut2 = [1:nGene[2] spins[2]]
+
+        writedlm(string(output1,"Net_",run,".txt"),sOut1)
+        writedlm(string(output2,"Net_",run,".txt"),sOut2)
     end
 end
-
-spins = [Initialize(nGene[i]) for i=1:2]
-
-for iter=1:1000000
-    success = 0
-    for e=1:epochs
-        passed = move()
-        success = success+passed
-    end
-    println(success/epochs)
-    if success/epochs < 0.01 #Stop iterations if there are very few successes
-        break
-    end
-    temp = tf(temp)
-    epochs = itf(epochs)
-end
-
-writedlm(output1,spins[1])
-writedlm(output2,spins[2])
