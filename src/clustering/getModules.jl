37028c7750f272c9e85bf04af257fab4256b0419
warnerm <mrwarner09@gmail.com>
2018-07-05 13:16:16 -0400

    fixed error holding while loop

--- src/clustering/getModules.jl
+++ src/clustering/getModules.jl
@@ -1,8 +1,10 @@
+# inputfile = "antOrthoNet_all_collectedRes.txt"
+# outputfile = "test"
 inputfile = ARGS[1]
 outputfile = ARGS[2]
 
 f = open(readdlm,inputfile)
-genes_remaining = [1:maximum(f[:,3:4])] #make list of all genes
+genes_remaining = collect(1:Int(maximum(f[:,3:4]))) #make list of all genes
 mod = 0
 ModDef = Vector{Int}(length(genes_remaining))
 
@@ -10,32 +12,36 @@ while length(genes_remaining) > 0
     println(length(genes_remaining))
     mod = mod + 1
     new_gene = genes_remaining[1]
-    ModDef[new_gene] = mod
+    splice!(genes_remaining,1)
     next_genes = []
     found = 0
     while true
-        conns = filter(x -> x[3] == new_gene && x[5] >= 0.95,f)
-        if size(conns,1) > 0
-            append!(next_genes,conns[:,4])
-            [ModDefModDef[conns[:,4]] .= mod #.= is the broadcasting assignment operator
+        ModDef[new_gene] = mod
+        conns = [Int(f[row,4]) for row in 1:size(f,1) if f[row,3] == new_gene && f[row,5] >= 0.95]
+        conns2 = [Int(f[row,3]) for row in 1:size(f,1) if f[row,4] == new_gene && f[row,5] >= 0.95]
+        append!(conns,conns2)
+        if length(conns) > 0
+            append!(next_genes,conns)
+        end
         while true
-            next = next_genes[1]
-            if next in genes_remaining
-                deleteat!(genes_remaining,findfirst(isequal(next),genes_remaining))
-                new_gene = next
-                popfirst!(next_genes)
-                found = 1
-                break
-            else if length(next_genes == 1)
+            if length(next_genes)==0
                 found = 0
                 break
+            end
+            next = next_genes[1]
+            if next in genes_remaining
+                filter!(x->x!=next,genes_remaining)
+                new_gene = next
+                splice!(next_genes,1)
+                found = 1
+                break
             else
-                popfirst!(next_genes)
+                splice!(next_genes,1)
             end
         end
         if found == 0
             break
-        end            
+        end
     end
 end
 
