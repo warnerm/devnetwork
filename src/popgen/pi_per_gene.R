3e34e7523ae543751a24539b36bbca92b54a92d8
warnerm <mrwarner09@gmail.com>
2018-09-04 14:20:54 -0400

    calculating average pi across genes

--- src/popgen/pi_per_gene.R
+++ src/popgen/pi_per_gene.R
@@ -2,11 +2,12 @@
 args = commandArgs(trailingOnly=TRUE)
 
 ann = read.csv(args[1],header=F)
-pi_site = read.csv(args[2],header=T)
+pi_site = read.table(args[2],header=T)
 pi_site = pi_site[!duplicated(pi_site),]
-pi_ann = merge(pi_site,ann,by.x=c("CHROM","POS"),by.y=c("V1","V2"))
-pi_mean = ddply(pi_site,~ V4,summarize,
+ann = ann[!duplicated(ann),]
+colnames(ann) = c("CHROM","POS","TYPE","Gene","Transcript")
+pi_ann = merge(pi_site,ann,by=c("CHROM","POS"))
+pi_mean = ddply(pi_site,~ Gene,summarize,
                 pi = mean(PI))
-colnames(pi_mean)[1] = "Gene"
 
 write.csv(pi_mean,file=args[2],row.names = FALSE)
