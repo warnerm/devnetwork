0989aa852b120bce6273cbe5cb1d523a220e9f66
warnerm <mrwarner09@gmail.com>
2018-09-06 10:22:50 -0400

    fix snipre command-line input

--- src/popgen/snipre.R
+++ src/popgen/snipre.R
@@ -13,15 +13,17 @@ source("snipre/my.jags2.R")
 print("Loading data")
 #load data
 
-annotation <- read.csv(args[1],header=F) # read data on mutation effect
+silentReplacement <- read.csv(args[1], header=T) # read expected silent replacement
+
+
+annotation <- read.csv(args[2],header=F) # read data on mutation effect
 colnames(annotation) <- c("chrom", "pos", "effect", "loc", "rna")
 annotation$pos <- paste(annotation$chrom,annotation$pos)
 annotation <- annotation[,-1]
-snps <- read.csv(args[2],header=F) # read fixed vs polymorphic
+snps <- read.csv(args[3],header=F) # read fixed vs polymorphic
 colnames(snps) <- c("chrom", "pos", "state")
 snps$pos <- paste(snps$chrom,snps$pos)
 snps <- snps[,-1]
-silentReplacement <- read.csv(args[3], header=T) # read expected silent replacement
 
 print("Data loaded")
 byPos <- merge(snps,annotation, by.x="pos", by.y="pos")
@@ -49,6 +51,6 @@ BSnIPRE.run(dnds, burnin = 10000, thin = 4, iter = 15000)
 load("samples")
 res <- BSnIPRE(samples, dnds)
 bres <- res$new.dataset
-write.csv(bres, file = "../out/bayesian_results.csv", row.names = FALSE)
+write.csv(bres, file = args[4], row.names = FALSE)
 
 
