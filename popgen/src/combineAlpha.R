da94c4b4d1367f23737dfcb4baf0b97e4f596705
warnerm <mrwarner09@gmail.com>
2018-09-13 15:27:08 -0400

    moved development up; renamed files

--- popgen/src/combineAlpha.R
+++ popgen/src/combineAlpha.R
@@ -9,7 +9,7 @@ collect <- function(stage,species){
   df <- read.csv(paste(inDir,"alpha_locusF_",stage,".",species,".csv",sep=""),sep="\t")
   df = df[-(nrow(df)),]
   df = df[,c("LnL","theta","tau","alpha.Cat1","alpha.Cat2","alpha.Cat3")]
-  df = df[-c(1,2),] 
+  df = df[1:2,]
   #alpha = apply(df[,c(4:6)],2,function(x) mean(as.numeric(as.character(x)))) #First line is the true values (no permutation test)
   #c1 = apply(df[,c(4:6)],2,function(x) quantile(x,0.025,na.rm=T)) #Note that every result is printed twice, but we don't really care because we are just looking for quantiles anyway
   #c2 = apply(df[,c(4:6)],2,function(x) quantile(x,0.975,na.rm=T))
