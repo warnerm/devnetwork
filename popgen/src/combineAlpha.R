743481e98f3d6b05fe36cd99d28b0b2bdc5d010c
warnerm <mrwarner09@gmail.com>
2018-09-17 14:41:55 -0400

    changed alpha collection to return all bootstrap results

--- popgen/src/combineAlpha.R
+++ popgen/src/combineAlpha.R
@@ -2,22 +2,16 @@
 args = commandArgs(trailingOnly=TRUE)
 library(plyr)
 
-inDir = args[1]
+prefix = args[1]
 outputFile = args[2]
 
 collect <- function(stage,species){
-  df <- read.csv(paste(inDir,"alpha_locusF_",stage,".",species,".csv",sep=""),sep="\t")
-  df = df[-(nrow(df)),]
-  df = df[,c("LnL","theta","tau","alpha.Cat1","alpha.Cat2","alpha.Cat3")]
-  df = df[1:2,]
-  #alpha = apply(df[,c(4:6)],2,function(x) mean(as.numeric(as.character(x)))) #First line is the true values (no permutation test)
-  #c1 = apply(df[,c(4:6)],2,function(x) quantile(x,0.025,na.rm=T)) #Note that every result is printed twice, but we don't really care because we are just looking for quantiles anyway
-  #c2 = apply(df[,c(4:6)],2,function(x) quantile(x,0.975,na.rm=T))
-  #d = as.data.frame(do.call(rbind,list(alpha,c1,c2)))
-  d = df
-  d$stage=stage
-  d$species=species
-  return(d)
+  df <- read.csv(paste(prefix,stage,".",species,".csv",sep=""),sep="\t")
+  df = df[-c(1:3),]
+  df = df[-seq(1,nrow(df) - 1, by=2),c("alpha.Cat1","alpha.Cat2","alpha.Cat3")]
+  df$stage=stage
+  df$species=species
+  return(df)
 }
 
 res = ldply(lapply(c("ant","bee"), function(x){
