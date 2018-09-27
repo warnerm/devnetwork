eef42e3c6003583e8e838b110c1015e8f2840b29
warnerm <mrwarner09@gmail.com>
2018-09-27 15:04:28 -0400

    working with map

--- popgen/src/MKalphaInput_newApis.R
+++ popgen/src/MKalphaInput_newApis.R
@@ -9,7 +9,7 @@ load("../data/DEtests.RData")
 beeSub <- read.csv(subFile)
 map = read.table(mapFile,head=F)
 colnames(map) = c("Gene","old_gene")
-beeSub = merge(beeSub,by="Gene")
+beeSub = merge(beeSub,map,by="Gene")
 
 mk = beeSub[,c(2,6,3,7,4,6,5,7)]
 mk = cbind(mk,rep(496,nrow(mk)),rep(1,nrow(mk)))
@@ -21,4 +21,4 @@ addClass <- function(DEdat){
   write.table(mk,file = paste(prefix,names(DEdat)[2],".bee.csv",sep = ""),col.names = FALSE,row.names = FALSE,sep=",")
 }
 
-sapply(c(2:ncol(beeRes[[2]])),function(x) addClass(beeRes[[2]][,c(1,x)],beeSub,beeMK,"bee"))
+sapply(c(2:ncol(beeRes[[2]])),function(x) addClass(beeRes[[2]][,c(1,x)]))
--- popgen/src/combineAlpha.R
+++ popgen/src/combineAlpha.R
@@ -6,12 +6,12 @@ prefix = args[1]
 outputFile = args[2]
 
 collect <- function(stage,species){
-  df <- read.csv(paste(prefix,stage,".",species,".csv",sep=""),sep="\t")
+  df <- read.csv(paste(prefix,stage,".",species,".csv",sep=""),sep="\t",head=T)
   #df = df[-c(1:3),]
   #df = df[-seq(1,nrow(df) - 1, by=2),c("alpha.Cat1","alpha.Cat2","alpha.Cat3")]
   df$stage=stage
   df$species=species
-  return(df)
+  return(as.data.frame(df[1,]))
 }
 
 res = ldply(lapply(c("bee"), function(x){
