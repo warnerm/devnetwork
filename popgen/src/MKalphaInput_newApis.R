2fee34b72e0845968e06b2f6f762b1e2b30fd4cf
warnerm <mrwarner09@gmail.com>
2018-09-27 14:02:06 -0400

    working with map

--- popgen/src/MKalphaInput_newApis.R
+++ popgen/src/MKalphaInput_newApis.R
@@ -3,33 +3,22 @@ args = commandArgs(trailingOnly=TRUE)
 
 prefix = args[1]
 subFile = args[2]
+mapFile = args[3]
 
 load("../data/DEtests.RData")
 beeSub <- read.csv(subFile)
-beeMK = beeSub[,c(2,6,3,7,4,6,5,7)]
-beeMK = cbind(beeMK,rep(22,nrow(beeMK)),rep(1,nrow(beeMK)))
+map = read.table(mapFile,head=F)
+colnames(map) = c("Gene","old_gene")
+beeSub = merge(beeSub,by="Gene")
 
-antEvol <- read.csv("../data/MpharAnn.csv")
-antEvol = antEvol[!is.na(antEvol$Fixed.Non.Synonymous),]
+mk = beeSub[,c(2,6,3,7,4,6,5,7)]
+mk = cbind(mk,rep(496,nrow(mk)),rep(1,nrow(mk)))
 
-antSub = antEvol[,c(1,13,17,12,16,15,17,14,16)]
-
-##44 chromosomes sampled (22 workers)
-antMK = cbind(antSub[,-c(1)],rep(44,nrow(antSub)),rep(1,nrow(antSub)))
-
-addClass <- function(DEdat,sub,mk,species){
+addClass <- function(DEdat){
   mk$V10 = rep(1,nrow(mk))
-  mk$V10[sub$Gene %in% DEdat$Gene[DEdat[,2]=="queen"]] = 2
-  mk$V10[sub$Gene %in% DEdat$Gene[DEdat[,2]=="worker"]] = 3
-  write.table(mk,file = paste(prefix,names(DEdat)[2],".",species,".csv",sep = ""),col.names = FALSE,row.names = FALSE,sep=",")
+  mk$V10[beeSub$old_gene %in% DEdat$Gene[DEdat[,2]=="queen"]] = 2
+  mk$V10[beeSub$old_gene %in% DEdat$Gene[DEdat[,2]=="worker"]] = 3
+  write.table(mk,file = paste(prefix,names(DEdat)[2],".bee.csv",sep = ""),col.names = FALSE,row.names = FALSE,sep=",")
 }
 
 sapply(c(2:ncol(beeRes[[2]])),function(x) addClass(beeRes[[2]][,c(1,x)],beeSub,beeMK,"bee"))
-sapply(c(2:ncol(antRes[[2]])),function(x) addClass(antRes[[2]][,c(1,x)],antSub,antMK,"ant"))
-
-#Make input from old data 
-antMK$class = 1
-antMK$class[antEvol$Gaster=="Reproductive"]=2
-antMK$class[antEvol$Gaster=="Worker"]=3
-
-write.table(antMK,file = "../MK_alpha_input/old_abd.csv",sep=",",row.names=FALSE,col.names=FALSE)
--- popgen/src/pairwiseBlast.py
+++ popgen/src/pairwiseBlast.py
@@ -2,13 +2,13 @@
 #SBATCH -p compute # partition (queue)
 #SBATCH --export=ALL
 #SBATCH -t 10-00:00
-#SBATCH -n 20
+#SBATCH -n 40
 
 import numpy as np, sys, re, getopt, pandas as pd
 import subprocess
 from subprocess import call
 from joblib import Parallel, delayed
-from Bio.Blast.Applications import NcbiblastnCommandline
+from Bio.Blast.Applications import NcbitblastxCommandline
 from Bio import SeqIO
 from Bio.Blast import NCBIXML
 
@@ -29,18 +29,19 @@ def InOut(argv):
 	                outfile = arg                         
 	return seqfile,dbfile,threads,outfile
 
-def callProc(gene,seqfile,dbfile):
+def callProc(gene,seqfile,dbfile,outfile):
 
 	#Make fasta file of the individual protein
 	seqiter = SeqIO.parse(open(seqfile),'fasta')
 	SeqIO.write((seq for seq in seqiter if seq.id in gene), "temp"+gene+".fa", "fasta")
 
-	blastp_cline = NcbiblastnCommandline(query="temp"+gene+".fa",db=dbfile,evalue=1e-10,
+	blastn_cline = NcbitblastxCommandline(query="temp"+gene+".fa",db=dbfile,evalue=1e-10,
 											outfmt=5,out="blast"+gene+".xml")
 
 	stdout, stderr = blastn_cline()
 
 	result_handle = open("blast"+gene+".xml")
+
 	blast_record = NCBIXML.read(result_handle)
 	E_VALUE_THRESH = 1e-10
 
@@ -51,6 +52,7 @@ def callProc(gene,seqfile,dbfile):
 					f.write(gene+'\t'+alignment.title+'\n')
 				break
 		break
+
 
 def main(argv):
 	seqfile,dbfile,threads,outfile = InOut(argv)
@@ -66,7 +68,7 @@ def main(argv):
 				l = l.replace("\n","")
 				seq_IDs = np.append(seq_IDs,l)
 
-	Parallel(n_jobs=int(threads))(delayed(callProc)(seq,seqfile,dbfile) for seq in seq_IDs)
+	Parallel(n_jobs=int(threads))(delayed(callProc)(seq,seqfile,dbfile,outfile) for seq in seq_IDs)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
