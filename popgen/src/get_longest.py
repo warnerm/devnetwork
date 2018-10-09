3015b2b8a35471c4deef8d518577465b0d585443
warnerm <mrwarner09@gmail.com>
2018-10-09 15:11:23 -0400

    got working

--- popgen/src/get_longest.py
+++ popgen/src/get_longest.py
@@ -3,9 +3,10 @@ import sys
 # take the output of gffread and find isoform length
 
 genes = SeqIO.to_dict(SeqIO.parse(sys.argv[1],"fasta"))
+outfile = sys.argv[2]
 
-with open(outfile,'a') as out:
+with open(outfile,'w') as out:
 	out.write("GeneID\tLength\n")
 	for rec in genes:
-		out.write(genes[rec].description.split("=")[0]+'\t'+str(len(rec))+'\n')
+		out.write(genes[rec].description.split(" ")[0]+'\t'+str(len(genes[rec]))+'\n')
 	
